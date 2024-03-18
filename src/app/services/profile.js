d7fce192c487151fbfef070fb7eb3f4a25df5019
IaberoEmp <87572596+IaberoEmp@users.noreply.github.com>
2024-03-18 00:36:50 -0300

    applyTags

--- src/app/services/profile.js
+++ src/app/services/profile.js
@@ -3,29 +3,32 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const profileApi = createApi({
     reducerPath:"profileApi",
     baseQuery:fetchBaseQuery({baseUrl:'https://miiaappcoder-default-rtdb.firebaseio.com'}),
+    tagTypes:["ProfileImage","UserLocation"],
     endpoints:(builder)=>({
         putProfileImage: builder.mutation({
             query:({profileImage,localId})=>({
                 url:`/profile/${localId}.json`,
                 method:"PUT",
                 body:{profileImage}
-            })
+            }),
+            invalidatesTags:["ProfileImage"]
         }),
         getProfileImage: builder.query({
-            query:(localId) => `/profile/${localId}.json`
+            query:(localId) => `/profile/${localId}.json`,
+            providesTags:["ProfileImage"]
         }),
         putUserLocation:builder.mutation({
             query:({localId,locationData})=> ({
                 url:`/userLocation/${localId}.json`,
                 method:"PUT",
                 body:locationData
-
-            })
+            }),
+            invalidatesTags:["UserLocation"]
         }),
         getUserLocation:builder.query({
-            query:(localId) => `/userLocation/${localId}.json`
+            query:(localId) => `/userLocation/${localId}.json`,
+            providesTags:["UserLocation"]
         })
-
     })
 })
 
